191c31850662650b7371f173ac3c66fa58c50bb6
Bry Jemi <bryand.bonaventura026@gmail.com>
2020-12-18 12:52:52 +0700

    init project

---
+++ js/script.js
@@ -0,0 +1,14 @@
+$("#nav a[href^='#']").on('click', function(e) {
+
+   // prevent default anchor click behavior
+   e.preventDefault();
+
+   // store hash
+   var hash = this.hash;
+
+   // animate
+   $('html, body').animate({
+       scrollTop: $(hash).offset().top-100
+     }, 1500, 'easeInOutExpo');
+
+});
